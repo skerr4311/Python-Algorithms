b98192f7783ee6abf605580c1578997a1cb7a435
Steven Kerr <44153164+skerr4311@users.noreply.github.com>
2019-08-07 11:05:05 +1200

    Update A1_sker078.py

--- A1_sker078.py
+++ A1_sker078.py
@@ -1,12 +1,9 @@
 import sys
-def main():
-    contents = sys.stdin.readlines()
-    arr = []
-    for i in contents:
-        text = i.rstrip("\n")
-        text = text.rstrip("#")
-        arr.append(text)
-    for x in arr:
-        print(x)
-if __name__=="__main__":
-    main()
+contents = sys.stdin.readlines()
+arr = []
+for i in contents:
+    text = i.rstrip()
+    text = text.rstrip("#")
+    arr.append(text)
+for x in arr:
+    print(x)

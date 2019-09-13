60d3a08bdaf7c28f1165b691f1c74246e63a2579
SkerrHome <sker078@aucklanduni.ac.nz>
2019-09-13 20:11:37 +1200

    i think i have it, going to try

---
+++ A2/A1_sker078.py
@@ -0,0 +1,35 @@
+import sys
+contents = sys.stdin.readlines()
+pointer = 0
+
+def shellSort(arr):
+    length = len(arr)
+    if length <= 1:
+        return arr
+    gap = length // 2
+    while gap > 0:
+        for start in range(gap):
+            gapSort(arr, start, gap)
+        gap = gap // 2
+
+def gapSort(arr, start, gap):
+    for i in range(start+gap, len(arr), gap):
+        currVal = arr[i]
+        position = i
+        while position >= gap and arr[position-gap]<currVal:
+            arr[position] = arr[position-gap]
+            position = position-gap
+        arr[position]=currVal
+
+#seperating out the read list
+while pointer <= len(contents)-1:
+    tempArr = []
+    if contents[pointer][0].isdigit():
+        tempArr = [int(x) for x in contents[pointer].split()]
+    else:
+        tempArr = contents[pointer].split()
+    pointer += 1
+    shellSort(tempArr)
+    for item in range(0, len(tempArr)-1):
+        print(tempArr[item]),
+    print(tempArr[len(tempArr)-1])

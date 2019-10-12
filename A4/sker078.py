e9673a14da3ef9963b5100c113cbc8c3907404dd
SkerrHome <sker078@aucklanduni.ac.nz>
2019-10-13 00:21:51 +1300

    psudo code done for Dijkstra's priority

--- A4/sker078.py
+++ A4/sker078.py
@@ -13,6 +13,31 @@ def Distance(lat1, lon1, lat2, lon2):
 
     return(R*c)
 
+def Dijkstra(matrix, length):
+    priorityQ = []
+    colour = ["WHITE"]*length
+    dist = [0]*length
+    colour[0] = "GREY"
+    priorityQ.append([0,0])
+    while priorityQ != []:
+        t1 = priorityQ[0][1] #value to city
+        u = priorityQ[0][0] #pointer to city
+        priorityQ.pop(0)
+        for x in range(0, length):
+            t2 = t1 + matrix[u][x]
+            if colour[x] == "WHITE":
+                colour[x] = "GREY"
+                priorityQ.append([x, t2])
+            elif colour[x] == "GREY":
+                for item in priorityQ: # need to make this more efficient
+                    if item[0] == x:
+                        if item[1] > t2:
+                            item[1] = t2
+        colour[u] = "BLACK"
+        dist[u] = t1
+    return(dist)
+
+
 
 def AdjacencyMatrix(length, gass, DicGraph):
     adjMatrix = []
@@ -42,8 +67,7 @@ while case_index != cases:
     DicGraph = {x: sys.stdin.readline().split() for x in range(cities)}
     gass = int(sys.stdin.readline())
     adjMatrix = AdjacencyMatrix(cities, gass, DicGraph)
-    for item in adjMatrix:
-        print(item)
+    print(Dijkstra(adjMatrix, cities))
     #send it off to shortest path algorithm
     case_index += 1
 
